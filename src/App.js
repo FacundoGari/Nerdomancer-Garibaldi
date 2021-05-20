bd4d608205f82ec9f7f0d485a02fddc62c08d55c
FacundoGari <facundogarib@gmail.com>
2021-05-20 15:27:58 -0300

    nerdomancer-garibaldi

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import 'core-js';
 import Navbar from './components/Navbar';
-import ItemCount from './components/ItemCount'
-import CardWidget from './components/CartWidget';
+//import ItemCount from './components/ItemCount'
+//import CardWidget from './components/CartWidget';
 import ItemListContainerCard from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
-    <h1>NerdoMancer!</h1>
-    <br></br>
     <ItemListContainerCard/>
+    <br></br>
+    <ItemCount/>
     </div>
+    
   );
 }
 
--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-//import CardGroup from 'react-bootstrap/Cardgroup';
+import CardGroup from 'react-bootstrap/Cardgroup';
 
 
 
@@ -18,7 +18,22 @@ const ItemCount = ({initial, stock}) => {
 
         return (
             <div>
-
+                <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="../print.jpg" width='100px' />
+            <Card.Body>
+                <Card.Title>Calavera</Card.Title>
+                <ButtonGroup>
+                <Button variant="dark">-</Button>
+                
+                <Button variant="dark">+</Button>
+                </ButtonGroup>
+                <Card.Text>
+                <p>Calavera impresa en 3D, puede ser blanca y negra.</p>
+                <i> La blanca se puede pintar del color que gustes!</i>
+                </Card.Text>
+                
+            </Card.Body>
+            </Card>
             </div>
         )
 }
